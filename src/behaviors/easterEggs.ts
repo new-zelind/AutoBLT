61070ca923e54b86fa4712e0665e44df40989e7f
new-zelind <zlin1055@gmail.com>
2020-08-13 23:05:23 -0400

    easter egg fixes

--- src/behaviors/easterEggs.ts
+++ src/behaviors/easterEggs.ts
@@ -1,21 +1,26 @@
 import { addMessageHandler } from "../lib/message";
 import { client } from "../client";
-import { PREFIX, isCommand } from "../lib/command";
-import { code } from "../lib/util";
-import { TextChannel, ClientUser } from "discord.js";
+import { Message } from "discord.js";
+
+const fuckYou:string[] = [
+  "Fuck you too! 😠",
+  "No, but thanks for the offer, I think?.",
+  "😳",
+  "What did I do to you?",
+  "Not cool!",
+  "Fuck you!",
+  "Should I be flattered?",
+  "Here, you might need one of these:\nhttps://cm.maxient.com/reportingform.php?ClemsonUniv&layout_id=1",
+  "...",
+  "I'm confused..."
+]
 
 //React with angry ping emote when pinged
 addMessageHandler((message) => {
   if (!client.user || !message.guild) return false;
-  if (message.mentions.users.has(client.user.id)) {
-    const pingEmote = message.guild.emojis.cache.find(
-      (emoji) => emoji.name === "ping"
-    );
-    if (pingEmote) {
-      message.react(pingEmote);
-    }
-    return true;
-  } else return false;
+  if (!message.mentions.users.has(client.user.id)) return false;
+  message.react("💢");    
+  return false
 });
 
 addMessageHandler((message) => {
@@ -23,4 +28,17 @@ addMessageHandler((message) => {
   if(!message.content.includes("cake" || "🎂")) return false;
   message.react("👀");  
   return true;
+});
+
+addMessageHandler((message) => {
+  if(!client.user || !message.mentions.users.has(client.user.id)){
+    return false;
+  }
+  if(!message.content.includes("fuck you")) return false;
+
+  message.channel.send(
+    `${fuckYou[Math.floor(Math.random() * (fuckYou.length - 1))]}`
+  );
+
+  return false;
 });
